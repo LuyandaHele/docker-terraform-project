dbd4f2e9724c32d5cc15d6892bef380c461ad491
LuyandaHele <luyandalui576@gmail.com>
2026-07-16 15:53:36 +0200

    rds file config

---
+++ rds.tf
@@ -0,0 +1,31 @@
+# creating database subnet group
+resource "aws_db_subnet_group" "database_subnet_group" {
+  name        = "${var.project_name}-${var.environment}-rds-subnets"
+  subnet_ids  = [aws_subnet.private_data_subnet_az1.id, aws_subnet.private_data_subnet_az2.id]
+  description = "subnets for rds"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-rds-subnets"
+  }
+}
+
+# launching a fresh rds instance
+resource "aws_db_instance" "database_instance" {
+  identifier             = var.database_instance_identifier
+  instance_class         = var.database_instance_class
+  engine                 = var.database_engine           
+  engine_version         = var.database_engine_version    
+  allocated_storage      = var.database_allocated_storage 
+  db_name                = var.database_name
+  username               = var.database_username
+  password               = var.database_password
+  db_subnet_group_name   = aws_db_subnet_group.database_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_security_group.id]
+  availability_zone      = data.aws_availability_zones.available_zones.names[1]
+  multi_az               = var.multi_az_deployment
+  skip_final_snapshot    = true
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-rds"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -55,3 +55,65 @@ variable "private_data_subnet_az2_cidr" {
     description = "Private Data Subnet CIDR block for AZ2"
     type = string 
 }
+
+# security groups variable
+variable "ssh_location" {
+    description = "ip address that can ssh into the servers"
+    type = string 
+}
+
+# rds variable for snapshot identifer
+variable "database_snapshot_identifier" {
+    description = "database snapshot name"
+    type = string
+}
+
+# rds variables for database instance class 
+variable "database_instance_class" {
+    description = "database instance type"
+    type = string
+}
+
+# rds variables for database instance identifer
+variable "database_instance_identifier" {
+    description = "database instance identifer"
+    type = string
+}
+
+# rds variables for database instance identifer
+variable "multi_az_deployment" {
+    description = "create a standby db instance"
+    type = bool
+    default = false
+}
+
+# rds other additional variables
+variable "database_engine"{
+    type = string
+    default = "PostgreSQL"
+}
+
+variable "database_engine_version"{
+    type = string
+}
+
+variable "database_allocated_storage"{
+    type = number 
+    default = 20
+}
+
+variable "database_username"{
+    type = string
+}
+
+variable "database_password"{
+    type = string 
+    sensitive = true
+}
+
+variable "database_name"{
+    type = string
+}
+
+
+
